fd344504f707d658ee442d3b397e3eeec4e4d195
Damon <voldev@hotmail.com>
2020-10-11 21:42:06 -0400

    -about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,13 @@ import SEO from "../components/seo"
 const About = () => {
   return (
     <Layout title={"About"}>
-      <SEO title="About" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+      <SEO title="About me" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-        praesentium in animi fugiat, aliquid corrupti voluptate fugit eaque quia
-        dolorum earum esse est quasi officiis sapiente ipsum sed culpa sint!
+        Hi! I'm Volkan. I started coding in 2015 by printing out the words 'Hello World!'. I got hooked so I began my journey with HTML, CSS and from there, moved to JavaScript. 
+        And I have not stopped ever since. I continue learning new technologies, languages and really enjoy creating exciting and hopefully useful things with that knowledge.
+        <br />
+        I live in Somerville, MA in the U.S. When I am not coding, I am probably laughing with my wife, hiking a mountain with a cool view, playing video games with my friends, 
+        or just biking around.   
       </p>
     </Layout>
   )
