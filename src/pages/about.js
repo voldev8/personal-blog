1b2f0369bd54927bbd8bf18fe18216efe48c3d5a
Damon <voldev@hotmail.com>
2021-01-28 16:41:31 -0500

    about, reponsive button and photo fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,8 @@ const About = () => {
         Hi! I'm Volkan. I started coding in 2015 by printing out the words 'Hello World!'. I got hooked so I began my journey with HTML, CSS and from there, moved to JavaScript. 
         And I have not stopped ever since. I continue learning new technologies, languages and really enjoy creating exciting and hopefully useful things with that knowledge.
         <br />
-        I live in Somerville, MA in the U.S. When I am not coding, I am probably laughing with my wife, hiking a mountain with a cool view, playing video games with my friends, 
+        I live in Somerville, MA in the U.S. When I am not coding, I am probably 
+        laughing with my wife, hiking a mountain with a cool view, playing video games with my friends, 
         or just biking around.   
       </p>
     </Layout>
